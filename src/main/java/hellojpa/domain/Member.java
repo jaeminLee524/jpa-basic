ac0e83af8713a02d1fd0b97c0108ced60b9378b7
jaeminLee524 <rkwoals524@hanmail.net>
2022-02-07 00:14:32 +0900

    embedded 타입의 period, address 추가

--- src/main/java/hellojpa/domain/Member.java
+++ src/main/java/hellojpa/domain/Member.java
@@ -3,20 +3,18 @@ package hellojpa.domain;
 import lombok.Data;
 
 import javax.persistence.*;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
 @Entity
 @Data
-public class Member extends BaseEntity{
+public class Member {
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "MEMBER_ID")
     private Long id;
     private String name;
-    private String city;
-    private String street;
-    private String zipcode;
 
     // 예시를 위해 추가
     @OneToMany(mappedBy = "member")
@@ -25,6 +23,15 @@ public class Member extends BaseEntity{
     //Member 1:N MemberProduct
     @OneToMany(mappedBy = "member")
     private List<MemberProduct> memberProducts = new ArrayList<>();
+
+    @Embedded
+    private Period workPeriod;
+
+    @Embedded
+    private Address homeAddress;
+
+
+
 
 //    @ManyToOne
 //    @JoinColumn(name = "team_id")
