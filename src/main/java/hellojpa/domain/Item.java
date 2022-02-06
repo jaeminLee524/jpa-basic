1fe427c9addc87b8afc327bbff45f6ba09b4df8e
jaeminLee524 <rkwoals524@hanmail.net>
2022-02-06 22:23:08 +0900

    슈퍼타입 서브타입 물리 모델 구현

--- src/main/java/hellojpa/domain/Item.java
+++ src/main/java/hellojpa/domain/Item.java
@@ -1,14 +1,20 @@
 package hellojpa.domain;
 
 import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
 import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
 
 @Entity
-@Data
-public class Item {
+@Getter @Setter
+@Inheritance(strategy = InheritanceType.JOINED)
+//@Inheritance(strategy = InheritanceType.TABLE_PER_CLASS) //각각 테이블로 만드는 속성
+//dtype
+@DiscriminatorColumn
+public abstract class Item {
 
     @Id @GeneratedValue
     @Column(name = "ITEM_ID")
@@ -20,4 +26,6 @@ public class Item {
 
     @ManyToMany(mappedBy = "items")
     private List<Category> categories = new ArrayList<>();
+
+    //dtype
 }
---
+++ src/main/java/hellojpa/domain/Movie.java
@@ -0,0 +1,24 @@
+package hellojpa.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.DiscriminatorValue;
+import javax.persistence.Entity;
+
+@Entity
+@Getter @Setter
+@DiscriminatorValue(value = "M")
+public class Movie extends Item {
+
+    private String director;
+    private String actor;
+
+//    @Override
+//    public String toString() {
+//        return "Movie{" +
+//                "director='" + director + '\'' +
+//                ", actor='" + actor + '\'' +
+//                '}';
+//    }
+}
